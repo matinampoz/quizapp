e986e8345833c9c8e47b89b2eeef2af06dfcb2fa
matinampoz <matinampoz@gmail.com>
2024-05-09 12:32:32 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ quizapp/src/main/java/com/myquiz/quizapp/DAO/QuizDAO.java
@@ -0,0 +1,9 @@
+package com.myquiz.quizapp.DAO;
+
+import com.myquiz.quizapp.Models.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizDAO extends JpaRepository<Quiz, Integer> {
+}
